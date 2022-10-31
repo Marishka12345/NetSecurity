b1c8c5fa8b91cc4006ef182635cf3fddf69ae0f5
Марина <marina.parshina.2003@mail.ru>
2022-10-31 15:57:25 +0500

    123

--- app/src/main/java/com/example/netsecuirity/MainActivity.kt
+++ app/src/main/java/com/example/netsecuirity/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.netsecuirity.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -39,11 +39,15 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.opredFragment3, R.id.ugrozaFragment2, R.id.protectFragment3, R.id.setFragment2,R.id.mobileFragment2, R.id.tipsFragment2,
+                R.id.parent_controlFragment2, R.id.userFragment2
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        with(navView){
+            setupWithNavController(navController)
+            setNavigationItemSelectedListener(this@MainActivity)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -56,29 +60,69 @@ class MainActivity : AppCompatActivity() {
 
         when(item.itemId) {
 
-                R.id.action_settings -> {
-                Toast.makeText(this,"127834", Toast.LENGTH_SHORT).show()
+            R.id.action_settings -> {
+                Toast.makeText(this, "127834", Toast.LENGTH_SHORT).show()
             }
-
         }
-
         return super.onOptionsItemSelected(item)
-
     }
 
 
     override fun onSupportNavigateUp(): Boolean {
-
-            Toast.makeText(this,"1234", Toast.LENGTH_SHORT).show()
-
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    override fun NavigationView.OnNavigationItemSelectedListener {
-        new Na
-
-
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        /*
+        *   Если совпадает по id, значит был нажат item ( id взял из activity_main_drawer )
+        *   Тут тебе нужно написать переход по нажатию
+        */
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return when(item.itemId){
+            R.id.opredFragment3 -> {
+                //Пример логики
+               navController.navigate(R.id.opredFragment3)
+                drawerLayout.close()
+                true
+            }
+            R.id.ugrozaFragment2 -> {
+                navController.navigate(R.id.ugrozaFragment2)
+                drawerLayout.close()
+                true
+            }
+            R.id.protectFragment3 -> {
+                navController.navigate(R.id.protectFragment3)
+                drawerLayout.close()
+                true
+            }
+            R.id.setFragment2 -> {
+                navController.navigate(R.id.setFragment2)
+                drawerLayout.close()
+                true
+            }
+            R.id.mobileFragment2 -> {
+                navController.navigate(R.id.mobileFragment2)
+                drawerLayout.close()
+                true
+            }
+            R.id.tipsFragment2 -> {
+                navController.navigate(R.id.tipsFragment2)
+                drawerLayout.close()
+                true
+            }
+            R.id.parent_controlFragment2 -> {
+                navController.navigate(R.id.parent_controlFragment2)
+                drawerLayout.close()
+                true
+            }
+            R.id.userFragment2 -> {
+                navController.navigate(R.id.userFragment2)
+                drawerLayout.close()
+                true
+            }
+            else -> false
+        }
     }
-
 }
--- app/src/main/java/com/example/netsecuirity/SplashActivity.kt
+++ app/src/main/java/com/example/netsecuirity/SplashActivity.kt
@@ -1,11 +1,16 @@
 package com.example.netsecuirity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.view.animation.TranslateAnimation
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +21,17 @@ class SplashActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
         setContentView(R.layout.activity_splash)
-
-        //Задержка на 4 секунды
+        planeta.startAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate))
         Handler().postDelayed({
+        //Задержка на 4 секунды
+        },1000)
+
+
             //Старт главной активности
+        Handler().postDelayed({
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             //И остановка splash активности
             finish()
-        },4000)
+        },6000)
     }
 }
